33ad83dedc153b18aa925bd6a476991050eebf8e
MatiasGabler <mngabler@uc.cl>
2023-11-08 18:29:49 -0300

    First Commit

---
+++ BowlEngine/BowlEngine.cpp
@@ -0,0 +1,12 @@
+﻿// BowlEngine.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "BowlEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
